71e162b91d4304896089254f9d3adb63ebc41c45
eliashezron <eliashezron23@gmail.com>
2021-07-15 19:42:40 +0300

    heroku deploy3

--- backend/server.js
+++ backend/server.js
@@ -47,6 +47,6 @@ if(process.env.NODE_ENV === 'production'){
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000 
 
 app.listen(PORT,console.log( `app is running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`.yellow.bold))
